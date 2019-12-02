9e88e54f84485aea5e52289a34c74619ef59466b
Albin Sturesson <albstu0208@skynet.halmstad.se>
2019-12-02 13:32:37 +0100

    bdfbhgf

---
+++ Positioner/Positioner/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Positioner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnPosition_Click(object sender, EventArgs e)
+        {
+            tbxPosition.Text = "";
+            Random MyRandom = new Random();
+            int tal = MyRandom.Next(0, 246);
+            string start = "sexpositions.club/positions/";
+            string slut = ".html";
+            tbxPosition.AppendText(start + tal + slut);
+            string url = start + tal + slut;
+            
+
+
+
+
+
+
+        }
+    }
+}
